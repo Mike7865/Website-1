5587037920f19d605739cb3ac7c1bfffe0da4ba9
Mike38 <mshar50055@gmail.com>
2019-09-19 23:43:04 +0300

    commit fifteen

--- script.js
+++ script.js
@@ -1,14 +1,32 @@
 var hamburgerButton = document.querySelector(".hamburger");
 var hamburgerMenu = document.querySelector(".nav");
 
+function closeHamburgerMenu() {
+    hamburgerMenu.classList.remove("active");
+    hamburgerButton.classList.remove("is-active");
+}
+
+function openHamburgerMenu() {
+    hamburgerMenu.classList.add("active");
+    hamburgerButton.classList.add("is-active");
+}
+
 hamburgerButton.addEventListener("click", function (e) {
     e.preventDefault();
     if(hamburgerMenu.classList.contains("active")) {
-        hamburgerMenu.classList.remove("active");
-        hamburgerButton.classList.remove("is-active");
+        closeHamburgerMenu();
     } else {
-        hamburgerMenu.classList.add("active");
-        hamburgerButton.classList.add("is-active");
+        openHamburgerMenu();
     }
 });
+
+hamburgerButton.addEventListener("click", function (e) {
+    e.preventDefault();
+    if(e.target.classList.contains("nav__link")) {
+        closeHamburgerMenu();
+    }
+});
+
+
+
 
