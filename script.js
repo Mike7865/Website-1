5f6f52d0de487c12fd2bcc3cb6cdb86acbe343de
Mike38 <mshar50055@gmail.com>
2019-09-22 20:51:50 +0300

    commit twenty three

--- script.js
+++ script.js
@@ -1,3 +1,112 @@
+//Scroll
+const sections = $(".section");
+const display = $(".maincontent");
+let inscroll = false;
+
+const mobileDetect = new MobileDetect(window.navigator.userAgent);
+const isMobile = mobileDetect.mobile();
+
+const countPositionPercent = sectionEq => {
+  return `${sectionEq * -100}%`;
+};
+
+const switchActiveClass = (elems, elemNdx) => {
+  elems
+    .eq(elemNdx)
+    .addClass("active")
+    .siblings()
+    .removeClass("active");
+};
+
+const unBlockScroll = () => {
+  setTimeout(() => {
+    inscroll = false;
+  }, 1300); // подождать пока завершится инерция на тачпадах
+};
+
+const performTransition = sectionEq => {
+  if (inscroll) return;
+  inscroll = true;
+
+  const position = countPositionPercent(sectionEq);
+  const switchFixedMenuClass = () =>
+    switchActiveClass($(".fixed-menu__item"), sectionEq);
+
+  switchActiveClass(sections, sectionEq);
+  switchFixedMenuClass();
+
+  display.css({
+    transform: `translateY(${position})`
+  });
+
+  unBlockScroll();
+};
+
+const scrollViewport = direction => {
+  const activeSection = sections.filter(".active");
+  const nextSection = activeSection.next();
+  const prevSection = activeSection.prev();
+
+  if (direction === "next" && nextSection.length) {
+    performTransition(nextSection.index());
+  }
+
+  if (direction === "prev" && prevSection.length) {
+    performTransition(prevSection.index());
+  }
+};
+
+$(document).on({
+  wheel: e => {
+    const deltaY = e.originalEvent.deltaY;
+    const direction = deltaY > 0 ? "next" : "prev";
+    scrollViewport(direction);
+  },
+  keydown: e => {
+    const tagName = e.target.tagName.toLowerCase();
+    const userTypingInInputs = tagName === "input" || tagName === "textarea";
+
+    if (userTypingInInputs) return;
+
+    switch (e.keyCode) {
+      case 40:
+        scrollViewport("next");
+        break;
+
+      case 38:
+        scrollViewport("prev");
+        break;
+    }
+  }
+});
+
+$("[data-scroll-to]").on("click", e => {
+  e.preventDefault();
+  performTransition(parseInt($(e.currentTarget).attr("data-scroll-to")));
+});
+
+// разрешаем свайп на мобильниках
+if (isMobile) {
+  window.addEventListener(
+    "touchmove",
+    e => {
+      e.preventDefault();
+    },
+    { passive: false }
+  );
+
+  $("body").swipe({
+    swipe: (event, direction) => {
+      let scrollDirecrion;
+      if (direction === "up") scrollDirecrion = "next";
+      if (direction === "down") scrollDirecrion = "prev";
+      scrollViewport(scrollDirecrion);
+    }
+  });
+}
+
+
+//Hamburgerbutton
 var hamburgerButton = document.querySelector(".hamburger");
 var hamburgerMenu = document.querySelector(".nav");
 
@@ -28,7 +137,7 @@ hamburgerButton.addEventListener("click", function (e) {
 });
 
 
-
+//Team__accordeon
 let team__accordeon = document.querySelector("#team__accordeon");
 
 team__accordeon.addEventListener("click", function (e) {
@@ -54,7 +163,7 @@ team__accordeon.addEventListener("click", function (e) {
 });
 
 
-
+//Menu__accordeon
 let menu__accordeon = document.querySelector("#menu__accordeon");
 
 menu__accordeon.addEventListener("click", function (e) {
@@ -80,7 +189,34 @@ menu__accordeon.addEventListener("click", function (e) {
 });
 
 
+//Reviews - overlay
+const openButton = document.querySelector("#btn--gray");
+const successOverlay = createOverlay("Подробнее");
 
+openButton.addEventListener("click", function() {
+  document.body.appendChild(successOverlay);
+});
+
+function createOverlay(content) {
+  const overlayElement = document.createElement("div");
+  overlayElement.classList.add("overlay");
+
+  const template = document.querySelector("#overlayTemplate");
+  overlayElement.innerHTML = template.innerHTML;
+
+  const closeElement = overlayElement.querySelector(".close");
+  closeElement.addEventListener("click", function() {
+    document.body.removeChild(overlayElement);
+  });
+
+  const contentElement = overlayElement.querySelector(".overlay__content");
+  contentElement.innerHTML = content;
+
+  return overlayElement;
+}
+
+
+//Slider
 const left = document.querySelector("#left");
 const right = document.querySelector("#right");
 const slider__list = document.querySelector("#slider__list");
@@ -108,7 +244,7 @@ left.addEventListener("click", function() {
 });
 
 
-
+//Form - отправка
 const form = document.querySelector("#form");
 const form__btn = document.querySelector("#form__btn");
 
@@ -143,35 +279,7 @@ form__btn.addEventListener("click", function(event) {
 });
 
 
-
-const openButton = document.querySelector("#btn--gray");
-const successOverlay = createOverlay("Подробнее");
-
-openButton.addEventListener("click", function() {
-  document.body.appendChild(successOverlay);
-});
-
-function createOverlay(content) {
-  const overlayElement = document.createElement("div");
-  overlayElement.classList.add("overlay");
-
-  const template = document.querySelector("#overlayTemplate");
-  overlayElement.innerHTML = template.innerHTML;
-
-  const closeElement = overlayElement.querySelector(".close");
-  closeElement.addEventListener("click", function() {
-    document.body.removeChild(overlayElement);
-  });
-
-  const contentElement = overlayElement.querySelector(".overlay__content");
-  contentElement.innerHTML = content;
-
-  return overlayElement;
-}
-
-
-
-
+//Form - заказ отправлен
 const openButton = document.querySelector("#form__btn");
 const successOverlay = createOverlay("Заказ отправлен");
 
@@ -198,7 +306,7 @@ function createOverlay(content) {
 }
 
 
-
+//Form - отправка не удалась
 const closeButton = document.querySelector("#form__btn");
 const unsuccessOverlay = createOverlay("Отправка не удалась");
 
